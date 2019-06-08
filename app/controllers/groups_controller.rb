f88140ca64968f97ab9931756a179f2aa0cc513d
harukahorii <annzuwatanukidayo@ezweb.ne.jp>
2019-06-08 13:40:20 +0900

    Create action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,13 @@
 class GroupsController < ApplicationController
+  def new
+  end
+
+  def update
+  end
+
+  def create
+  end
+
+  def edit
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
-  resources :users, only: [:edit, :updete]
+  resources :users, only: [:index, :edit, :updete]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
   end
