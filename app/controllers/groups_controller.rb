fb07882ac04f88c4e051f56d92204580c6ac2150
harukahorii <annzuwatanukidayo@ezweb.ne.jp>
2019-06-10 11:17:55 +0900

    Create group files

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,15 +2,22 @@ class GroupsController < ApplicationController
   before_action :set_group, only: [:edit, :update]
   def index
   end
+
   def new
     @group = Group.new
     @group.users << current_user
   end
 
-  def update
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
   end
 
-  def create
+  def update
     if @group.update(group_params)
       redirect_to group_messages_path(@group), notice: 'グループを編集しました'
     else
@@ -18,10 +25,8 @@ class GroupsController < ApplicationController
     end
   end
 
-  def edit
-  end
-
   private
+
   def group_params
     params.require(:group).permit(:name, { :user_ids => [] })
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def update
-    if currebt_user.update(user_params)
+    if current_user.update(user_params)
       redirect_to root_path
     else
       render :edit
