15d6aa16ea91e8c42d1f465b4ebf583ee611036f
harukahorii <annzuwatanukidayo@ezweb.ne.jp>
2019-06-19 14:44:13 +0900

    Update messages_controller

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,7 +1,6 @@
 class Api::MessagesController < ApplicationController
   def index
-    @group = Group.find(params[:group_id])
-    @messages = @group.messages.includes(:user).where('id > ?', params[:id])
+    @messages = Group.find(params[:group_id]).messages.includes(:user).where('id > ?', params[:id])
 
   end
 end
