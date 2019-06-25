90157503597c801c5ca500eaba829089ed0c4107
harukahorii <annzuwatanukidayo@ezweb.ne.jp>
2019-06-25 15:11:43 +0900

    Update user.js

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -3,7 +3,7 @@ $(function() {
   var user_list = $("#user-search-result");
 
   function appendUser(user) {
-    var html = `<div class="chat-group-user clearfix" id='chat-group-user-${user.id}'>
+    var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${ user.name }</p>
                   <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</div>
                 </div>`
@@ -11,14 +11,14 @@ $(function() {
   }
 
   function appendErrMsgToHTML(msg) {
-    var html = `<div class="chat-group-user clearfix" id='chat-group-user-${user.id}'>
+    var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${ msg }</p>
                 </div>`
     user_list.append(html);
   }
 
   function buildHTML(id, name) {
-    var html = `<div class="chat-group-user clearfix js-chat-member" id="chat-group-user-${id}">
+    var html = `<div class="chat-group-user clearfix js-chat-member" id="${id}">
                   <input name="group[user_ids][]" type="hidden" value="${id}">
                   <p class="chat-group-user__name">${ name }</p>
                   <a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn">削除</a>
@@ -42,6 +42,7 @@ $(function() {
       if (users.length !== 0) {
         users.forEach(function(user){
           appendUser(user);
+          console.log(user)
         });
       }
       else {
