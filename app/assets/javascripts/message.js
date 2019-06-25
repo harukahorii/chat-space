b15ba77c01d7c280851f45c2515c2cf4419d7e80
harukahorii <annzuwatanukidayo@ezweb.ne.jp>
2019-06-25 12:25:53 +0900

    Update gemfile

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,75 +1,73 @@
-$(document).on('turbolinks:load', function(){
-  $(function() {
+$(function() {
 
-    function buildMESSAGES(message) {
-      var image = (message.image)? `<img class="lower-message__image" src="${message.image}" >`: "";
+  function buildMESSAGES(message) {
+    var image = (message.image)? `<img class="lower-message__image" src="${message.image}" >`: "";
 
-      var html = `<div class="message" data-id="${message.id}">
-                    <div class="upper-message">
-                      <div class="upper-message__user-name">
-                        ${message.user_name}
-                      </div>
-                      <div class="upper-message__date">
-                        ${message.created_at}
-                      </div>
+    var html = `<div class="message" data-id="${message.id}">
+                  <div class="upper-message">
+                    <div class="upper-message__user-name">
+                      ${message.user_name}
                     </div>
-                    <div class="lower-message">
-                      <p class="lower-message__content">
-                        ${message.content}
-                      </p>
+                    <div class="upper-message__date">
+                      ${message.created_at}
                     </div>
-                    ${image}
-                  </div>`
-      return html;
-    }
+                  </div>
+                  <div class="lower-message">
+                    <p class="lower-message__content">
+                      ${message.content}
+                    </p>
+                  </div>
+                  ${image}
+                </div>`
+    return html;
+  }
 
-    $('#new_message').on('submit', function(e) {
-      e.preventDefault();
-      var formData = new FormData(this);
-      var url = $(this).attr('action')
+  $('#new_message').on('submit', function(e) {
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data) {
+      var html = buildMESSAGES(data);
+      $('.messages').append(html)
+      $('.new_message')[0].reset();
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast')
+      $('.form__submit').attr('disabled', false);
+    })
+    .fail(function(){
+      alert('error');
+    })
+  });
+
+  var reloadMessages = (function() {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)) {
+      var last_message_id = $('.message:last').data('id');
       $.ajax({
-        url: url,
-        type: 'POST',
-        data: formData,
+        url: "api/messages",
+        type: 'get',
         dataType: 'json',
-        processData: false,
-        contentType: false
+        data: {id: last_message_id}
       })
-      .done(function(data) {
-        var html = buildMESSAGES(data);
-        $('.messages').append(html)
-        $('.new_message')[0].reset();
-        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast')
-        $('.form__submit').attr('disabled', false);
+      .done(function(messages) {
+        var insertHTML = '';
+        messages.forEach(function(message) {
+          insertHTML = buildMESSAGES(message);
+          $('.messages').append(insertHTML);
+          $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+        })
+        
       })
       .fail(function(){
-        alert('error');
+        alert('自動更新に失敗しました');
       })
-    });
-
-    var reloadMessages = (function() {
-      if (window.location.href.match(/\/groups\/\d+\/messages/)) {
-        var last_message_id = $('.message:last').data('id');
-        $.ajax({
-          url: "api/messages",
-          type: 'get',
-          dataType: 'json',
-          data: {id: last_message_id}
-        })
-        .done(function(messages) {
-          var insertHTML = '';
-          messages.forEach(function(message) {
-            insertHTML = buildMESSAGES(message);
-            $('.messages').append(insertHTML);
-            $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-          })
-          
-        })
-        .fail(function(){
-          alert('自動更新に失敗しました');
-        })
-      }
-    })
-    setInterval(reloadMessages, 5000);
-  });
+    }
+  })
+  setInterval(reloadMessages, 5000);
 });
--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -21,9 +21,10 @@ $(function() {
     var html = `<div class="chat-group-user clearfix js-chat-member" id="chat-group-user-${id}">
                   <input name="group[user_ids][]" type="hidden" value="${id}">
                   <p class="chat-group-user__name">${ name }</p>
-                  <a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn" data-user-id="${id}">削除</a>
+                  <a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn">削除</a>
                 </div>`
-    return html
+    $("#chat-group-users").append(html);
+    
   }
 
   $("#user-search-field").on("keyup", function() {
@@ -59,9 +60,8 @@ $(function() {
       $(this).parent('.chat-group-user').remove();
     })
 
-    $("#chat-group-users").on('click', '.user-search-remove', function() {
-      var id = $(this).data('user-id');
-      $(`#chat-group-user-${id}`).remove();
+    $("#chat-group-users").on('click', '.chat-group-user__btn--remove', function() {
+      $(this).parent().remove();
     })
   });
 });
